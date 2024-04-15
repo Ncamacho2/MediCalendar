de34675b77e2855ca3cf245866ef889ea233f121
Nelson Camacho <ccan999@hotmail.com>
2024-04-15 17:54:43 -0500

    perfil y cambio de video.mp4

--- app/src/main/java/com/poligran/medicalendar/ui/web/WebFragment.kt
+++ app/src/main/java/com/poligran/medicalendar/ui/web/WebFragment.kt
@@ -1,6 +1,7 @@
 package com.poligran.medicalendar.ui.web
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,8 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.EditText
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.poligran.medicalendar.R
@@ -42,25 +45,38 @@ class WebFragment : Fragment() {
         editTextUrl = view.findViewById(R.id.editTextUrl)
         buttonGo = view.findViewById(R.id.buttonGo)
         webView = view.findViewById(R.id.webView)
-
+        editTextUrl.setImeActionLabel("Hecho", KeyEvent.KEYCODE_ENTER);
+        editTextUrl.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE ||
+                (event != null && event.keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN)) {
+                // Validar el texto al presionar Enter
+                val texto = editTextUrl.text.toString()
+                if (!texto.startsWith("https://")) {
+                    editTextUrl.setText("https://$texto")
+                    editTextUrl.setSelection(editTextUrl.text.length)
+                }
+                true
+            }
+            false
+        }
         buttonGo.setOnClickListener {
             loadUrl()
         }
-        editTextUrl.setOnFocusChangeListener { v, hasFocus ->
-            if (!hasFocus && !editTextUrl.text.toString().startsWith("http://") && !editTextUrl.text.toString().startsWith("https://")) {
-                editTextUrl.setText("https://${editTextUrl.text}")
-            }
-        }
+
 
         return view
     }
 
+
     private fun loadUrl() {
         val url = editTextUrl.text.toString().trim()
+        webView.visibility = View.VISIBLE // Mostrar el WebView
+        webView.webViewClient = WebViewClient()
         if (url.isNotEmpty()) {
-            webView.visibility = View.VISIBLE // Mostrar el WebView
-            webView.webViewClient = WebViewClient()
+
             webView.loadUrl(url)
+        } else {
+            webView.loadUrl("https://google.com")
         }
     }
 }
