1aff26e793dfcfa058d6a787e675032fca94e7b4
Nelson Camacho <ccan999@hotmail.com>
2024-04-18 17:28:17 -0500

    citas oscar

--- app/src/main/java/com/poligran/medicalendar/ui/inicio/InicioFragment.kt
+++ app/src/main/java/com/poligran/medicalendar/ui/inicio/InicioFragment.kt
@@ -1,12 +1,19 @@
 package com.poligran.medicalendar.ui.inicio
 
-import androidx.fragment.app.viewModels
+
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.viewModels
 import com.poligran.medicalendar.R
+import com.poligran.medicalendar.ui.perfil.ProfileFragment
+
 
 class InicioFragment : Fragment() {
 
@@ -16,16 +23,31 @@ class InicioFragment : Fragment() {
 
     private val viewModel: InicioViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
+        val view = inflater.inflate(R.layout.fragment_inicio, container, false)
+        val botonCambio = view.findViewById<Button>(R.id.buttonIniciarSesion)
+        botonCambio.setOnClickListener {
+            val builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("Bienvenido")
+            builder.setMessage("¡Hola! Bienvenido MediCalendar.")
+            builder.setPositiveButton("Aceptar") { dialogInterface: DialogInterface, _: Int ->
+
+                dialogInterface.dismiss()
+
+            }
+            val dialog = builder.create()
+            dialog.show()
+        }
+        return view
     }
+
+    fun navegar(view: View) {
+
+    }
+
 }
